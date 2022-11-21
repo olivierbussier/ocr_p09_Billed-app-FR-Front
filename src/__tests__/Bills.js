0148b2d5e59015df7193c990277675e925ac0faf
Olivier Bussier <olivier@bussier.fr>
2022-11-21 15:16:48 +0100

    Expect sentence manquante dans le test Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -31,7 +31,7 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
+      expect(screen.getByTestId('icon-window').classList.contains('active-icon')).toBeTruthy()
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
